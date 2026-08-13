5e53109f839d9b19e71eed70ce517947e3e77cba
efraclab <pabhijit087@gmail.com>
2026-08-13 18:16:13 +0530

    Carbohydrate

--- frontend/src/models/PreparationData.ts
+++ frontend/src/models/PreparationData.ts
@@ -21,6 +21,7 @@ export interface PreparationData {
         | "protein"
         | "sugar"
         | "energy"
+        | "carbohydrate"
         | "roi"
         | "sulphated_ash"
         | "titration"
